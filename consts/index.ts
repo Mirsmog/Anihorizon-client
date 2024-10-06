3ad6a3b91858511dff6027654d72b488510ad3c4
zwine <tanharka.tyuchiev4@mail.ru>
2024-10-06 11:50:16 +0500

    feat: add meta consts

---
+++ consts/index.ts
@@ -0,0 +1,5 @@
+export const SITE_META_INFO = {
+	name: 'Anihorizon',
+	description:
+		"Anihorizon offers an unparalleled streaming experience for anime enthusiasts. With a vast library of anime titles ranging from timeless classics to the latest releases, Anihorizon ensures that every fan finds their favorite genre and series in exceptional quality. Whether you're looking to relive nostalgic episodes or explore new adventures, Anihorizon is the ultimate destination for all your anime streaming needs."
+}
